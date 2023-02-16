75674d005988b7f628100f340fd758f10ba0b33f
leeheejuuun <leehee117500@gmail.com>
2023-02-17 00:09:20 +0900

    udemy-react-query 17장

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query'; // 설명 : 서버에서 데이터를 가져올 때 사용하는 훅
+import { useQuery, useMutation } from 'react-query'; // 설명 : 서버에서 데이터를 가져올 때 사용하는 훅
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -29,6 +29,11 @@ export function PostDetail({ post }) {
     ['comments', post.id], //  post.id는 의존성 배열 처럼 작동한다. 문자열 'comments' 에 식별자가 추가된 셈이다.
     () => fetchComments(post.id)
   );
+
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+
+  const updateMutation = useMutation((postId) => updatePost(postId));
+
   if (isLoading) return <h3>Loading...</h3>;
   if (isError)
     return (
@@ -40,7 +45,29 @@ export function PostDetail({ post }) {
   return (
     <>
       <h3 style={{ color: 'blue' }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && (
+        <p style={{ color: 'red' }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: 'purple' }}>Deleting the post</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post has (not) been deleted</p>
+      )}
+      <button onClick={() => updateMutation.mutate(post.id)}>
+        Update title
+      </button>
+      {updateMutation.isError && (
+        <p style={{ color: 'red' }}>Error updating the post</p>
+      )}
+      {updateMutation.isLoading && (
+        <p style={{ color: 'purple' }}>Updating the post</p>
+      )}
+      {updateMutation.isSuccess && (
+        <p style={{ color: 'green' }}>Post has (not) been updated</p>
+      )}
+
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (
