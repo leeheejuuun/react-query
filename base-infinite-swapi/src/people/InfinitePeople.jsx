24951053f077b76f49db59cc7f7817d463dea7e9
leeheejuuun <leehee117500@gmail.com>
2023-02-17 17:17:54 +0900

    udemy-react-query 27장

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,13 +1,55 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Person } from "./Person";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Person } from './Person';
 
-const initialUrl = "https://swapi.dev/api/people/";
+import { useInfiniteQuery } from 'react-query';
+
+const initialUrl = 'https://swapi.dev/api/people/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  // fetchNextPage는 더 많은 데이터가 필요할 때 어느 함수를 실행할지를 infiniteScroll에 지시하는 역할을 한다.
+  // hasNextPage는 수집할 데이터가 더 있는지를 결정하는 불리언이다.
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isError,
+    error,
+    isFetching,
+  } = useInfiniteQuery(
+    'sw-people',
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    // useInfiniteQuery의 모든것은 pageParam에 달려 있다. pageParam은 fetchNextPage가 어떻게 보일지 결정하고 다음 페이지가 있는지 결정한다.
+
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error!{error.toString()}</div>;
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) => {
+          return pageData.results.map((person) => {
+            // pageData는 각 페이지에 useInfiniteQuery가 fetchNextPage로 불러온 데이터 배열을 맵핑한다.
+            return (
+              <Person
+                key={person.name}
+                name={person.name}
+                hairColor={person.hair_color}
+                eyeColor={person.eye_color}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
--- base-infinite-swapi/src/species/InfiniteSpecies.jsx
+++ base-infinite-swapi/src/species/InfiniteSpecies.jsx
@@ -1,13 +1,52 @@
-import InfiniteScroll from "react-infinite-scroller";
-import { Species } from "./Species";
+import InfiniteScroll from 'react-infinite-scroller';
+import { Species } from './Species';
+import { useInfiniteQuery } from 'react-query';
 
-const initialUrl = "https://swapi.dev/api/species/";
+const initialUrl = 'https://swapi.dev/api/species/';
 const fetchUrl = async (url) => {
   const response = await fetch(url);
   return response.json();
 };
 
 export function InfiniteSpecies() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery(
+    'sw-species',
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    // 위 쿼리 함수는 useInfiniteQuery의 pageParam을 쓰는데 initialUrl에서 시작하여 pageParam값으로 fetchUrl을 실행한다.
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+      // getNextPageParam는 이전페이지의 다음 프로퍼티를 불러와서 새 페이지 데이터가 있을 때마다 pageParam에 지정 해준다.
+    }
+  );
+
+  if (isLoading) return <div className="loading">Loading...</div>;
+  if (isError) return <div>Error!{error.toString()}</div>;
+  return (
+    <>
+      {isFetching && <div className="loading">Loading...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {/* hasMore함수는 infiniteScroll 컴포넌트가 계속 데이터를 불러올지를 결정하는 역할을 한다.*/}
+        {data.pages.map((pageData) => {
+          return pageData.results.map((species) => {
+            return (
+              <Species
+                key={species.name}
+                name={species.name}
+                language={species.language}
+                averageLifespan={species.average_lifespan}
+              />
+            );
+          });
+        })}
+      </InfiniteScroll>
+    </>
+  );
 }
